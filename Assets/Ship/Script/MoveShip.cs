f581e0f03b929289089a01e09df87b750cc8f467
JCJ05 <julitoaguerorengifo@hotmail.com>
2023-04-15 18:29:55 -0500

    Shooter Blue

--- Assets/Ship/Script/MoveShip.cs
+++ Assets/Ship/Script/MoveShip.cs
@@ -11,6 +11,8 @@ public class MoveShip : MonoBehaviour
     public float limiteInferior;
     public float limiteSuperior;
     public float velocidadFondoVertical = 1f;
+    public GameObject laserPrefab;
+    public float laserSpeed;
 
     // Start is called before the first frame update
     void Start()
@@ -46,6 +48,15 @@ public class MoveShip : MonoBehaviour
         {
             movimientoVertical = velocidadVertical * Time.deltaTime;
         }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+{
+    // Crea una instancia del prefab del láser en la posición actual de la nave
+    GameObject laserInstance = Instantiate(laserPrefab, transform.position, Quaternion.identity);
+
+    // Agrega una velocidad hacia arriba al Rigidbody2D del láser
+    laserInstance.GetComponent<Rigidbody2D>().velocity = new Vector2(0, laserSpeed);
+}
 
         float nuevaPosicionX = Mathf.Clamp(transform.position.x + movimientoHorizontal, limiteIzquierdo, limiteDerecho);
         float nuevaPosicionY = Mathf.Clamp(transform.position.y + movimientoVertical, limiteInferior, limiteSuperior);
