ec36b172f75a127d4d675d512a04cdeb58b2cefa
JCJ05 <julitoaguerorengifo@hotmail.com>
2023-04-15 18:14:38 -0500

    Limites Movimiento

--- Assets/Ship/Script/MoveShip.cs
+++ Assets/Ship/Script/MoveShip.cs
@@ -6,10 +6,20 @@ public class MoveShip : MonoBehaviour
 {
     public float velocidadHorizontal = 5f;
     public float velocidadVertical = 5f;
+    public float limiteIzquierdo;
+    public float limiteDerecho;
+    public float limiteInferior;
+    public float limiteSuperior;
+    public float velocidadFondoVertical = 1f;
 
     // Start is called before the first frame update
     void Start()
     {
+
+        limiteInferior = -4.399358f;
+        limiteSuperior = 4.278425f;
+        limiteIzquierdo = -6.92664f;
+        limiteDerecho = 6.829587f;
         
     }
 
@@ -37,7 +47,11 @@ public class MoveShip : MonoBehaviour
             movimientoVertical = velocidadVertical * Time.deltaTime;
         }
 
-        transform.Translate(new Vector3(movimientoHorizontal, movimientoVertical, 0f));
+        float nuevaPosicionX = Mathf.Clamp(transform.position.x + movimientoHorizontal, limiteIzquierdo, limiteDerecho);
+        float nuevaPosicionY = Mathf.Clamp(transform.position.y + movimientoVertical, limiteInferior, limiteSuperior);
+
+        transform.position = new Vector3(nuevaPosicionX, nuevaPosicionY, transform.position.z);
+        
 
     }
 }
